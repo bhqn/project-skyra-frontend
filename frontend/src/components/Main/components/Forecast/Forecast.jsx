f6daa48f5e35e1625e72d95916d9b5fffaac5808
bhqn <bernardo_quintanilha@hotmail.com>
2026-01-24 00:52:06 -0300

    previsao

--- frontend/src/components/Main/components/Forecast/Forecast.jsx
+++ frontend/src/components/Main/components/Forecast/Forecast.jsx
@@ -1,9 +1,49 @@
 import "./Forecast.css"
+import ForecastImage from "../../../../assets/sunny.svg?react"
 function Forecast (){
     return(
         <section className="forecast">
-            <p>Previsão da semana</p>
-            <div className="forecast__card"></div>
+            <p className="forecast__title">Previsão da semana</p>
+            <div className="forecast__card">
+                <div className="forecast__card-iten">
+                    <p className="forecast__card-title">Segunda</p>
+                    <ForecastImage className="forecast__card-image"></ForecastImage>
+                    <p className="forecast__card-temp">32C°</p>
+                </div>
+                <div className="forecast__card-iten">
+                    <p className="forecast__card-title">Terça</p>
+                    <ForecastImage className="forecast__card-image"></ForecastImage>
+                    <p className="forecast__card-temp">32C°</p>
+                </div>
+                <div className="forecast__card-iten">
+                    <p className="forecast__card-title">Quarta</p>
+                    <ForecastImage className="forecast__card-image"></ForecastImage>
+                    <p className="forecast__card-temp">32C°</p>
+                </div>
+                <div className="forecast__card-iten">
+                    <p className="forecast__card-title">Quinta</p>
+                    <ForecastImage className="forecast__card-image"></ForecastImage>
+                    <p className="forecast__card-temp">32C°</p>
+                </div>
+                <div className="forecast__card-iten">
+                    <p className="forecast__card-title">Sexta</p>
+                    <ForecastImage className="forecast__card-image"></ForecastImage>
+                    <p className="forecast__card-temp">32C°</p>
+                </div>
+                   <div className="forecast__card-iten">
+                    <p className="forecast__card-title">Sabado</p>
+                    <ForecastImage className="forecast__card-image"></ForecastImage>
+                    <p className="forecast__card-temp">32C°</p>
+                </div>
+                <div className="forecast__card-iten">
+                    <p className="forecast__card-title">Domingo</p>
+                    <ForecastImage className="forecast__card-image"></ForecastImage>
+                    <p className="forecast__card-temp">32C°</p>
+                </div>
+                
+                
+
+            </div>
         </section>
     )
 }
